94568b9418648921ed10f95db13d9a09d035d6bf
Nico Arcamone <nicolas.arcamone7@gmai.com>
2024-04-10 19:15:38 -0300

    solucion error lista favs y diseños de cards

--- @angular-bookstore/src/app/components/list-favoritos/list-favoritos.component.ts
+++ @angular-bookstore/src/app/components/list-favoritos/list-favoritos.component.ts
@@ -10,26 +10,36 @@ import { Libro } from 'src/interfaces/libro.interface';
 })
 export class ListFavoritosComponent implements OnInit{
 
+  // varible vacia tipo user
   usuarioActivo?: Usuario 
+
+  // lista vacia de libros
   libros?: Libro[] 
 
   constructor(private usuarioService: UsuarioService){}
 
   ngOnInit(): void {
     this.obtenerUsuario(1)
+   
   }
 
-
+  // obtener un user id desde el back
   obtenerUsuario(id:number){
      this.usuarioService.obtenerUsuario(id).subscribe( (data: Usuario) => {
         this.usuarioActivo = data
      })
   }
 
+  // obtener los libros del usuario acitvo
+  librosDeUsuario() {
+    this.libros = this.usuarioActivo?.libros
+    return this.libros
+  }
+
 
   eliminarDeListaFavoritos(usuarioId:number, libroId:number){
     this.usuarioService.eliminarLibro(usuarioId,libroId).subscribe( () => {
-      console.log('Libro eliminado con exito');
+        this.obtenerUsuario(usuarioId)
     })
   }
 }
